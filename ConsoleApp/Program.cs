3cf9740f68418c8c85ec7a19396678176cda59c8
David-ImSorry <jd.barbetti@gmail.com>
2024-04-11 21:56:21 -0500

    Program (Completo)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,22 @@
-﻿using MyLibrary;
+﻿
+using MyLibrary;
 
-int i = 5;
-string Name = "HELLO!!!";
+Employee Maria = new Secretary();
+Maria.Name = "Maria";
 
-Product Leche;
-Leche = new Product(1, "Leche Entera", 19, 100);
+Employee Pedro = new Manager();
+Pedro.Name = "Pedro";
+
+Developer Miguel = new Developer();
+Console.WriteLine($"Dias: {Miguel.GetPaymentDays()}");
+
+WriteEmployeeInfo(Maria);
+WriteEmployeeInfo(Miguel);
+WriteEmployeeInfo(Pedro);
+
+//Polimorfismo
+void WriteEmployeeInfo(Employee employee)
+{
+    Console.WriteLine($"{employee.Name} : {employee.CalculateMonthlyPayment()}");
+    Console.WriteLine($"Days: {employee.GetPaymentDays()}");
+}
